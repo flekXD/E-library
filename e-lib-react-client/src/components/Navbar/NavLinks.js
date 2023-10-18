b1db3e0f04392f8372a8e1be97017322dff023ac
flekXD <ipz211_kos@student.ztu.edu.ua>
2023-10-18 10:58:12 +0300

    123

---
+++ e-lib-react-client/src/components/Navbar/NavLinks.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavLinks.css"
+//import { useDispatch, useSelector } from 'react-redux';
+//import {logout} from '../../reducers/userReducer'
+
+const NavLinks = props => {
+    //const isAuth = useSelector(state => state.user.isAuth)
+    //const dispatch = useDispatch()
+    const isAuth = true;
+
+    return (
+    <ul className="nav-links">
+        <li>
+            <NavLink to="/">Catalog</NavLink>
+        </li>
+        {isAuth &&
+        <li>
+            <NavLink to="/product/add">Add Product</NavLink>
+        </li>
+        }
+        {isAuth &&<li>
+            <NavLink to="/user/cart">Cart</NavLink>
+        </li> }
+        {!isAuth && <li>
+            <NavLink to="/user/login">Login</NavLink>
+        </li>}
+        {isAuth && <li>
+            <NavLink to='/user/logout' onClick={()=> dispatch(logout())}>logout</NavLink>
+        </li>}
+    </ul>
+    )
+};
+
+export default NavLinks;
